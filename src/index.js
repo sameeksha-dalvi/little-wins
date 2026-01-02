d4b21c2489e6587aa14214b6dbd6e0523f5830e0
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-01-02 20:25:47 +0530

    feat: open add todo dialog on button click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+
+const addTodoBtn = document.querySelector("#add-todo-btn");
+const todoModal = document.querySelector("#add-todo-modal");
+const closetodoModal = document.querySelector("#close-add-todo");
+
+addTodoBtn.addEventListener('click', function () {
+    todoModal.showModal();
+});
+
+closetodoModal.addEventListener('click',function(){
+    todoModal.close();
+})
